7cbfed8fc2cf33440d27e82610b8cf62d3d865dc
csw <2480311255@qq.com>
2023-06-08 09:11:34 +0800

    初始化项目

---
+++ src/main/java/com/yupi/springbootinit/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.InterfaceInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author csw
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2023-06-07 09:37:06
+* @Entity com.yupi.springbootinit.model.entity.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/springbootinit/service/impl/InterfaceInfoServiceImpl.java
@@ -0,0 +1,154 @@
+package com.yupi.springbootinit.service.impl;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yupi.springbootinit.common.ErrorCode;
+import com.yupi.springbootinit.constant.CommonConstant;
+import com.yupi.springbootinit.exception.BusinessException;
+import com.yupi.springbootinit.exception.ThrowUtils;
+import com.yupi.springbootinit.model.dto.interfaceinfo.InterfaceInfoQueryRequest;
+import com.yupi.springbootinit.model.entity.InterfaceInfo;
+import com.yupi.springbootinit.model.entity.User;
+import com.yupi.springbootinit.model.vo.InterfaceInfoVO;
+import com.yupi.springbootinit.model.vo.UserVO;
+import com.yupi.springbootinit.service.InterfaceInfoService;
+import com.yupi.springbootinit.mapper.InterfaceInfoMapper;
+import com.yupi.springbootinit.service.UserService;
+import com.yupi.springbootinit.utils.SqlUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+* @author csw
+* @description 针对表【interface_info(接口信息)】的数据库操作Service实现
+* @createDate 2023-06-07 09:37:06
+*/
+@Service
+public class InterfaceInfoServiceImpl extends ServiceImpl<InterfaceInfoMapper, InterfaceInfo>
+    implements InterfaceInfoService{
+
+    @Resource
+    private UserService userService;
+
+    @Override
+    public void validInterfaceInfo(InterfaceInfo interfaceInfo, boolean add) {
+
+        if (interfaceInfo == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        String name = interfaceInfo.getName();
+        String description = interfaceInfo.getDescription();
+        String url = interfaceInfo.getUrl();
+        String requestParams = interfaceInfo.getRequestParams();
+        String requestHeader = interfaceInfo.getRequestHeader();
+        String responseHeader = interfaceInfo.getResponseHeader();
+        Integer status = interfaceInfo.getStatus();
+        String method = interfaceInfo.getMethod();
+        Long userId = interfaceInfo.getUserId();
+        Date createTime = interfaceInfo.getCreateTime();
+        Date updateTime = interfaceInfo.getUpdateTime();
+        Integer isDelete = interfaceInfo.getIsDelete();
+
+        // 创建时，参数不能为空
+        if (add) {
+            ThrowUtils.throwIf(StringUtils.isAnyBlank(name, description, url, requestParams, method), ErrorCode.PARAMS_ERROR);
+        }
+        // 有参数则校验
+        if (StringUtils.isNotBlank(name) && name.length() > 50) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "名称过长");
+        }
+    }
+
+    /**
+     * 获取查询包装类
+     *
+     * @param interfaceInfoQueryRequest
+     * @return
+     */
+    @Override
+    public QueryWrapper<InterfaceInfo> getQueryWrapper(InterfaceInfoQueryRequest interfaceInfoQueryRequest) {
+        QueryWrapper<InterfaceInfo> queryWrapper = new QueryWrapper<>();
+        if (interfaceInfoQueryRequest == null) {
+            return queryWrapper;
+        }
+
+        String name = interfaceInfoQueryRequest.getName();
+        String description = interfaceInfoQueryRequest.getDescription();
+//     String url = interfaceInfoQueryRequest.getUrl();
+//     String requestParams = interfaceInfoQueryRequest.getRequestParams();
+//     String requestHeader = interfaceInfoQueryRequest.getRequestHeader();
+//     String responseHeader = interfaceInfoQueryRequest.getResponseHeader();
+//     Integer status = interfaceInfoQueryRequest.getStatus();
+//     String method = interfaceInfoQueryRequest.getMethod();
+//     Integer isDelete = interfaceInfoQueryRequest.getIsDelete();
+        String searchText = interfaceInfoQueryRequest.getSearchText();
+        String sortField = interfaceInfoQueryRequest.getSortField();
+        String sortOrder = interfaceInfoQueryRequest.getSortOrder();
+        Long id = interfaceInfoQueryRequest.getId();
+        Long userId = interfaceInfoQueryRequest.getUserId();
+        // 拼接查询条件
+        if (StringUtils.isNotBlank(searchText)) {
+            queryWrapper.like("name", searchText).or().like("description", searchText);
+        }
+        queryWrapper.like(StringUtils.isNotBlank(name), "name", name);
+        queryWrapper.like(StringUtils.isNotBlank(description), "description", description);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(id), "id", id);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(userId), "userId", userId);
+        queryWrapper.eq("isDelete", false);
+        queryWrapper.orderBy(SqlUtils.validSortField(sortField), sortOrder.equals(CommonConstant.SORT_ORDER_ASC),
+                sortField);
+        return queryWrapper;
+    }
+    @Override
+    public InterfaceInfoVO getInterfaceInfoVO(InterfaceInfo interfaceInfo, HttpServletRequest request) {
+        InterfaceInfoVO interfaceInfoVO = InterfaceInfoVO.objToVo(interfaceInfo);
+        // 1. 关联查询用户信息
+        Long userId = interfaceInfo.getUserId();
+        User user = null;
+        if (userId != null && userId > 0) {
+            user = userService.getById(userId);
+        }
+        UserVO userVO = userService.getUserVO(user);
+        interfaceInfoVO.setUser(userVO);
+        return interfaceInfoVO;
+    }
+
+    @Override
+    public Page<InterfaceInfoVO> getInterfaceInfoVOPage(Page<InterfaceInfo> interfaceInfoPage, HttpServletRequest request) {
+        List<InterfaceInfo> interfaceInfoList = interfaceInfoPage.getRecords();
+        Page<InterfaceInfoVO> interfaceInfoVOPage = new Page<>(interfaceInfoPage.getCurrent(), interfaceInfoPage.getSize(), interfaceInfoPage.getTotal());
+        if (CollectionUtils.isEmpty(interfaceInfoList)) {
+            return interfaceInfoVOPage;
+        }
+        // 1. 关联查询用户信息
+        Set<Long> userIdSet = interfaceInfoList.stream().map(InterfaceInfo::getUserId).collect(Collectors.toSet());
+        Map<Long, List<User>> userIdUserListMap = userService.listByIds(userIdSet).stream()
+                .collect(Collectors.groupingBy(User::getId));
+        // 填充信息
+        List<InterfaceInfoVO> interfaceInfoVOList = interfaceInfoList.stream().map(interfaceInfo -> {
+            InterfaceInfoVO interfaceInfoVO = InterfaceInfoVO.objToVo(interfaceInfo);
+            Long userId = interfaceInfo.getUserId();
+            User user = null;
+            if (userIdUserListMap.containsKey(userId)) {
+                user = userIdUserListMap.get(userId).get(0);
+            }
+            interfaceInfoVO.setUser(userService.getUserVO(user));
+            return interfaceInfoVO;
+        }).collect(Collectors.toList());
+
+        interfaceInfoVOPage.setRecords(interfaceInfoVOList);
+        return interfaceInfoVOPage;
+    }
+}
+
+
+
+
